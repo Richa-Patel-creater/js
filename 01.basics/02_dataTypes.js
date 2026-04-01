a6261b7698ab1b93e449d012933fc2b2377d218c
Richa Patel <patelricha7188@gmail.com>
2026-04-01 07:25:44 +0000

    dataypes

---
+++ 01.basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using nodejs not browser
+
+let name = "richa"  //string
+let age = 22        //number
+let isLoggedIn = false  //boolean
+
+const foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
+
+
+console.log(typeof null)        //object
+console.log(typeof undefined)   //undefined
+
+/**
+ * javascript documentation- MDN (not official)
+ *                         - ecma (official)
+ */
+
+/*
+Primitive dataypes-
+number
+bigint 
+string
+boolean
+null
+undefined 
+symbol
+
+
+object
+*/
