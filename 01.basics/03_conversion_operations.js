235eefbe3d67288d1bce1e9dcd527d6fa32b8bf0
Richa Patel <patelricha7188@gmail.com>
2026-04-01 12:45:33 +0000

    operators

---
+++ 01.basics/03_conversion_operations.js
@@ -0,0 +1,53 @@
+let stringScore = "33"
+
+//console.log(typeof stringScore) //string
+
+let numberScore= Number (stringScore)
+
+//console.log(typeof numberScore) //number
+
+
+let val="abc"
+console.log(typeof val) //string
+let newVal= Number (val)
+// console.log(typeof newVal) //number
+// console.log(newVal)  //NaN
+
+
+/*
+to number
+"33" => 33
+"33abs" => NaN
+true => 1
+false => 0
+null => 0
+undefined => NaN
+
+
+to boolean
+1 => true
+0 => false
+345 =>true
+"" =>false
+"abs" => true
+*/
+
+//********************************Operations***********************************//
+
+/*
+
+2+2
+2-2
+2*4
+2**3
+9/3
+9%3
+
+"hello"+"richa"===>"helloricha"
+
+1+"2"      //12
+"1"+2      //12
+"1"+2+2    //122 
+1+2+"2"    //32
+*/
+
