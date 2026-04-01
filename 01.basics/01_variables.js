88d91fd1438835349cd588d5b1b0b714eb394b9a
Richa Patel <patelricha7188@gmail.com>
2026-04-01 07:03:19 +0000

    variables in java

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 25371
+let accountEmail = "richa@gamil.com"
+var accountPassword = "123"
+accountCity = "jaipur" //not prefered
+let accountState;
+
+//accountId=56793 --->const cant be changed
+accountEmail = "hell@gamil.com"
+accountPassword = "1226273"
+accountCity = "pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/**
+ * prefer not to use var
+ * because of block scope and functional scope
+ */
