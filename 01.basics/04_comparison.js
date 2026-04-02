852f778785c8fa36faecfd453952e0a6c3f645dc
Richa Patel <patelricha7188@gmail.com>
2026-04-02 07:24:30 +0000

    summary of dataypes

---
+++ 01.basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(null==0); //false //works different
+console.log(null>0);
+console.log(null>=0); //true//js converts null to zero in case of comparison
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined >=0);
+
+console.log("02" > 1);   //true
+console.log(2 > "1");   //true
+console.log("2" == 2);  //true
+console.log("2" === 2); //false
