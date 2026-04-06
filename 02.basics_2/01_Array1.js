1babec27084e91373e04db28cd53b12bd02b5664
Richa Patel <patelricha7188@gmail.com>
2026-04-06 16:28:31 +0000

    arrays

---
+++ 02.basics_2/01_Array1.js
@@ -0,0 +1,38 @@
+const myArr = [1,2,3,4,5,"richa",true]
+const myArr1 = new Array(1,2,3,4);
+const myArr2 = ["thor","hulk"]
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// console.log(myArr);
+
+const arr=[0,1,2,3,4,5];
+console.log(arr);
+
+//arr.unshift(0);
+//arr.shift();
+
+// console.log(arr.includes(6))
+// console.log(arr.indexOf(6))
+
+// const newArr=arr.join();
+// console.log(arr)
+// console.log(newArr)
+// console.log(typeof newArr) //string
+
+const newArr1=arr.slice(1,3)
+console.log(newArr1);
+console.log(arr);
+
+const newArr2=arr.splice(1,3)
+console.log(newArr2);
+console.log(arr);
+
+
+
+
+
+
+
+
